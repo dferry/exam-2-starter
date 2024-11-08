d74ecde9c1ef77c921fb86e3cca0b4cbe2157b8b
David Ferry <dferry@slu.edu>
2024-11-08 11:35:33 -0600

    Create simpleBlackjack.m

---
+++ simpleBlackjack.m
@@ -0,0 +1,15 @@
+function avgScore = simpleBlackjack(limit, trials)
+%
+% This function simulates playing many hands of
+% Simple Blackjack where the player continually
+% hits up to a given limit. Final score for each
+% hand is computed, and the average score over all
+% hands is returned.
+%
+% INPUT limit – The player always hits if their
+% hand value is less than limit
+% INPUT trials – The number of hands to simulate
+%
+% OUTPUT avgScore – The average final score of
+% all hands.
+
