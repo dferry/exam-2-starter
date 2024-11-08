485b412f61848a579096438b8baa8680b5493766
David Ferry <dferry@slu.edu>
2024-11-08 11:34:32 -0600

    Create fruit.m

---
+++ fruit.m
@@ -0,0 +1,7 @@
+function [] = fruit( N )
+% This function prints for values from 1 to N:
+% “apple” if the value is divisible by 2
+% “banana” if the value is divisible by 5
+% The number itself if neither is true
+% “applebanana” if both are true
+
